1e4d1d76e07900cb67ce56c7bb2ce416ddfc284f
Luis Aguilar <luisdiegoaguilar@outlook.com>
2021-03-12 22:57:30 -0600

    Starting firebase integration for the backend

--- src/App.js
+++ src/App.js
@@ -8,14 +8,8 @@ function App() {
   return (
     // BEM
     <div className="app">
-
-    {/* Sidebar */}
     <Sidebar />
-
-    {/* Feed */}
     <Feed />
-
-    {/* Widgets */}
     <Widgets />
     </div>
   );
